5b36ba262c20976da347b1e717bfa8ced26e148b
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:45:28 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/lineage_caymanslm.mk
+    $(LOCAL_DIR)/lmodroid_caymanslm.mk
--- lmodroid_caymanslm.mk
+++ lmodroid_caymanslm.mk
@@ -25,13 +25,13 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 $(call inherit-product, device/lge/caymanslm/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device identifiers
-PRODUCT_NAME := lineage_caymanslm
+PRODUCT_NAME := lmodroid_caymanslm
 PRODUCT_DEVICE := caymanslm
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
